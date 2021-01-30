22e257c6ecaaf346ca00c66fb5d2e70083c3439f
James Morgan <james.morgan.e@googlemail.com>
2021-01-30 16:45:20 +0000

    Adding marketplace TODOs

--- contracts/marketplace/SteppedPrimarySaleMarketplace.sol
+++ contracts/marketplace/SteppedPrimarySaleMarketplace.sol
@@ -15,9 +15,13 @@ import "hardhat/console.sol";
 contract SteppedPrimarySaleMarketplace is Context {
     using SafeMath for uint256;
 
+    event SaleStarted(uint256 indexed editionId, uint256 basePrice, uint256 stepPrice);
+
     struct Price {
+        // TODO combine price and step into a single slot
         uint256 basePrice;
         uint256 stepPrice;
+        uint256 currentStep;
     }
 
     KOAccessControls public accessControls;
@@ -30,24 +34,30 @@ contract SteppedPrimarySaleMarketplace is Context {
         koda = _koda;
     }
 
+    // TODO allow creator to call this director or always force through a factory?
+    //      - maybe expose a method for this option in the future regardless?
+
+    // TODO handle start date?
+
     // TODO handle zero/free price
 
-    function setupSale(uint256 editionId, uint256 basePrice, uint256 stepPrice) public view {
+    function setupSale(uint256 _editionId, uint256 _basePrice, uint256 _stepPrice) public {
         require(accessControls.hasContractRole(_msgSender()), "KODA: Caller must have smart contract role");
-        require(pricing[editionId].basePrice == 0, "Marketplace: edition already setup");
+        require(pricing[_editionId].basePrice == 0, "Marketplace: edition already setup");
+
+        pricing[_editionId] = Price(_basePrice, _stepPrice);
+
+        emit SaleStarted(_editionId, _basePrice, _stepPrice);
+    }
+
+    function makePurchase(uint256 _editionId) public payable {
 
     }
 
-    function updateSale(uint256 editionId, uint256 basePrice, uint256 stepPrice) public view {
+    function updateSale(uint256 _editionId, uint256 _basePrice, uint256 _stepPrice) public {
 
     }
 
-    function cancelSale(uint256 editionId) public view {
-
+    function cancelSale(uint256 _editionId) public {
     }
-
-    function makePurchase(uint256 editionId) public payable {
-
-    }
-
 }
