ab49e5deb60b71dce720c628af0d597c54792b17
James Morgan <james.morgan.e@googlemail.com>
2021-02-11 18:13:10 +0000

    Bug Fixes

--- test/deployer/ContractDeployer.vanityAddress.test.js
+++ test/deployer/ContractDeployer.vanityAddress.test.js
@@ -33,15 +33,35 @@ contract('MinterFactory', function (accounts) {
     await this.accessControls.grantRole(this.MINTER_ROLE, artist, {from: deployer});
   });
 
+  // based on this https://solidity-by-example.org/app/create2/
+
   it.skip('generates a nice address', async () => {
     const deployerFactory = await ContractDeployer.new();
 
     const bytes = await deployerFactory.getKODACreationBytecode(this.accessControls.address, ZERO_ADDRESS, STARTING_EDITION, {from: deployer});
 
-    for (let i = 0; i <= 10000; i++) {
+    // Create 2 factory address [0x000223E151A44FAabDa3f9AAf400a6EC9f7fBb50] | salt [7546] | length [19]
+    // Create 2 factory address [0x0000d64Bf16bD4f9058AeBBb4c3bd2Cd291fCA4E] | salt [32653] | length [18]
+
+    let foundLength = 20; // default starting size of 20
+    let winningSalt = 0;
+    let winningAddress = 0;
+
+    for (let i = 100000; i <= 500000; i++) {
       const address = await deployerFactory.deploy.call(bytes, ethers.utils.formatBytes32String(i.toString()));
-      console.log(`Create 2 factory address [${address}] | salt [${i}]`);
+
+      const length = ethers.utils.stripZeros(address).length;
+      if (length <= foundLength) {
+        foundLength = length;
+        winningSalt = i;
+        winningAddress = address;
+        console.log(`Match create 2 factory address [${address}] | salt [${i}] | length [${length}]`);
+      }
     }
+
+    console.log(`
+      Smallest contract found was [${winningAddress}] with salt [${winningSalt}] and length [${foundLength}]
+    `)
 
   }).timeout("10000000");
 
