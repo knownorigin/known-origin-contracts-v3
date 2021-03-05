ecae3946c74dff4243bbdbba8fa8811de29e519c
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-03-05 19:16:18 +0000

    domain hash

--- contracts/marketplace/KODAV3SignatureMarketplace.sol
+++ contracts/marketplace/KODAV3SignatureMarketplace.sol
@@ -15,6 +15,12 @@ import { IKODAV3 } from "../core/IKODAV3.sol";
 contract KODAV3SignatureMarketplace is ReentrancyGuard, Context {
     using SafeMath for uint256;
 
+    // Contract name
+    string public constant name = "KODAV3SignatureMarketplace";
+
+    // marketplace version
+    string public constant version = "3";
+
     // edition buy now
     event EditionPurchased(uint256 indexed _editionId, uint256 indexed _tokenId, address indexed _buyer, uint256 _price);
 
@@ -25,6 +31,36 @@ contract KODAV3SignatureMarketplace is ReentrancyGuard, Context {
 
     // keccak256("Permit(address _creator,address _editionId,uint256 _price,address _paymentToken,uint256 _startDate,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0xf3382276f6888783a091a3aaa8e9e7f25b042fdb1cf10e366884472180bbdcf6;
+
+    // FIXME get GAS costings for using a counter and draw down method for KO funds?
+    // platform funds collector
+    address public platformAccount;
+
+    IKODAV3 public koda;
+    IKOAccessControlsLookup public accessControls;
+
+    constructor(
+        IKOAccessControlsLookup _accessControls,
+        IKODAV3 _koda,
+        address _platformAccount
+    ) {
+        koda = _koda;
+        platformAccount = _platformAccount;
+        accessControls = _accessControls;
+
+        // Grab chain ID
+        uint256 chainId;
+        assembly {chainId := chainid()}
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                chainId,
+                address(this)
+            ));
+    }
 
     function isListingValid(
         address _creator,
@@ -118,12 +154,33 @@ contract KODAV3SignatureMarketplace is ReentrancyGuard, Context {
         );
     }
 
-    function facilitateNextPrimarySale(
-        uint256 _editionId,
-        uint256 _paymentAmount,
-        address _buyer
-    ) internal returns (uint256) {
-        // todo copy pasta back code from original marketplace
-        return 0;
+    function facilitateNextPrimarySale(uint256 _editionId, uint256 _paymentAmount, address _buyer) internal returns (uint256) {
+        // get next token to sell along with the royalties recipient and the original creator
+        (address receiver, address creator, uint256 tokenId) = koda.facilitateNextPrimarySale(_editionId);
+
+        // split money
+        // todo
+        //handleEditionSaleFunds(receiver, _paymentAmount);
+
+        // send token to buyer (assumes approval has been made, if not then this will fail)
+        koda.safeTransferFrom(creator, _buyer, tokenId);
+
+        // FIXME we could in theory remove this
+        //      - and use the current approach of KO where a bidder must pull back any funds once its sold out on primary
+        //      - would probs shave a good bit of GAS (profile the options)
+        //      - could be replaced with a open method when in that state, monies are returned to bidder (future proof building tools to cover this)
+
+        //todo: sell out logic
+        // if we are about to sellout - send any open offers back to the bidder
+//        if (tokenId == koda.maxTokenIdOfEdition(_editionId)) {
+//
+//            // send money back to top bidder if existing offer found
+//            Offer storage offer = editionOffers[_editionId];
+//            if (offer.offer > 0) {
+//                _refundBidder(offer.bidder, offer.offer);
+//            }
+//        }
+
+        return tokenId;
     }
 }
