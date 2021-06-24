f362bebf5b5b82262b5742a130487a6c4c43e7b3
James Morgan <james.morgan.e@googlemail.com>
2021-06-24 09:34:42 +0100

    Missed commit

--- contracts/core/IHasSecondarySaleFees.sol
+++ contracts/core/IHasSecondarySaleFees.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.5;
 
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-contract IHasSecondarySaleFees is IERC165 {
+interface IHasSecondarySaleFees is IERC165 {
 
     event SecondarySaleFees(uint256 tokenId, address[] recipients, uint[] bps);
 
-    function getFeeRecipients(uint256 id) public view returns (address payable[] memory);
+    function getFeeRecipients(uint256 id) external returns (address payable[] memory);
 
-    function getFeeBps(uint256 id) public view returns (uint[] memory);
+    function getFeeBps(uint256 id) external returns (uint[] memory);
 }
