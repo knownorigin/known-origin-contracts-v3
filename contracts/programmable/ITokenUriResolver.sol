144183e1c333958b0839cf1cbf80fdad363e7b48
James Morgan <james.morgan.e@googlemail.com>
2021-06-25 15:26:17 +0100

    Updaing comments

--- contracts/programmable/ITokenUriResolver.sol
+++ contracts/programmable/ITokenUriResolver.sol
@@ -2,10 +2,6 @@
 
 pragma solidity 0.8.4;
 
-/// @title A ERC-721 compliant contract which has a focus on being GAS efficient along with being able to support
-/// both unique tokens and multi-editions sharing common traits but of limited supply
-///
-/// @author KnownOrigin Labs - https://knownorigin.io/
 interface ITokenUriResolver {
 
     /// @notice Return the edition or token level URI - token level trumps edition level if found
