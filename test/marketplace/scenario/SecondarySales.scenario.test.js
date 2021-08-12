8559362f0d60161057ad25cefe7ef12e837d0b27
James Morgan <james.morgan.e@googlemail.com>
2021-08-12 21:12:24 +0100

    Adding bug fix and scenario testing

--- test/marketplace/scenario/SecondarySales.scenario.test.js
+++ test/marketplace/scenario/SecondarySales.scenario.test.js
@@ -61,20 +61,15 @@ contract('Secondary market sales scenarios', function (accounts) {
     await this.token.setApprovalForAll(this.marketplace.address, true, {from: collectorA});
     await this.token.setApprovalForAll(this.marketplace.address, true, {from: collectorB});
     await this.token.setApprovalForAll(this.marketplace.address, true, {from: collectorC});
-
-    // Mint an edition of 3
-    await this.token.mintBatchEdition(3, minter, TOKEN_URI, {from: contract});
+    await this.token.setApprovalForAll(this.marketplace.address, true, {from: collectorD});
   });
 
   // scenario 1 (list and relist)
-  // mint primary
-  // list & sell secondary
-  // list and sell a second time
   describe('scenario 1 - mint, list and sell, relist and sell on secondary', () => {
-
-    const _0_1_ETH = ether('0.1');
-
     it('can fulfil scenario', async () => {
+      // Mint an edition of 3
+      await this.token.mintBatchEdition(3, minter, TOKEN_URI, {from: contract});
+
       // Mint and sell 1 token to collector A
       await this.primaryMarketplace.listForBuyNow(minter, firstEditionTokenId, _0_1_ETH, '0', {from: contract});
       await this.primaryMarketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
@@ -92,27 +87,116 @@ contract('Secondary market sales scenarios', function (accounts) {
     });
   });
 
-  // scenario 2 (list and transfer)
-  // mint primary
-  // list of secondary
-  // transfer to owner
-  // list and sell as new owner
+  // scenario 2 (list and transfer, relist)
   describe('scenario 2 - mint, list and sell, transfer then relist and sell on secondary', () => {
     it('can fulfil scenario', async () => {
 
+      // Mint an edition of 3
+      await this.token.mintBatchEdition(3, minter, TOKEN_URI, {from: contract});
+
+      // Mint and sell 1 token to collector A
+      await this.primaryMarketplace.listForBuyNow(minter, firstEditionTokenId, _0_1_ETH, '0', {from: contract});
+      await this.primaryMarketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorA);
+
+      // lists and sell from A -> B
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorA});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorB, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorB);
+
+      // new owner B lists and then transfers to C
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorB});
+      await this.token.transferFrom(collectorB, collectorC, firstEditionTokenId, {from: collectorB});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorC);
+
+      // list and sell from collector C to collector D
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorC});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorD, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorD);
     });
   });
 
-  // scenario 3 (mixed listing)
-  // mint primary
-  // list & sell on secondary
-  // list and sell as reserve
-  // list & sell on secondary
-  // list and sell as reserve from new buyer
-  describe('scenario 1 - list and sell in various types', () => {
+  // scenario 3 (mixed listing with transfer in the middle)
+  describe('scenario 3 - list and sell in various types with transfer mid-flow', () => {
     it('can fulfil scenario', async () => {
 
+      // Mint an edition of 3
+      await this.token.mintBatchEdition(3, minter, TOKEN_URI, {from: contract});
+
+      // Mint and sell 1 token to collector A
+      await this.primaryMarketplace.listForBuyNow(minter, firstEditionTokenId, _0_1_ETH, '0', {from: contract});
+      await this.primaryMarketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorA);
+
+      // lists and sell from A -> B
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorA});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorB, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorB);
+
+      // list as reserve and sell B -> C
+      await this.marketplace.listForReserveAuction(collectorB, firstEditionTokenId, ether('0.25'), '0', {from: collectorB});
+      await this.marketplace.placeBidOnReserveAuction(firstEditionTokenId, {from: collectorC, value: ether('0.3')});
+
+      // increase time beyond end
+      const {biddingEnd} = await this.marketplace.editionOrTokenWithReserveAuctions(firstEditionTokenId);
+      await time.increaseTo(biddingEnd);
+
+      // result reserve auction - new owner of C
+      await this.marketplace.resultReserveAuction(firstEditionTokenId);
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorC);
+
+      // C lists item but then transfer to D
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorC});
+      await this.token.transferFrom(collectorC, collectorD, firstEditionTokenId, {from: collectorC});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorD);
+
+      // D final lists and sells back to A
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorD});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorA);
     });
   });
 
+  // scenario 3 (mixed listing with transfers after revere with emergency exit and further listing)
+  describe('scenario 4 - mixed listing with transfer after bidding has ended', () => {
+    it('can fulfil scenario', async () => {
+
+      // Mint an edition of 3
+      await this.token.mintBatchEdition(3, minter, TOKEN_URI, {from: contract});
+
+      // Mint and sell 1 token to collector A
+      await this.primaryMarketplace.listForBuyNow(minter, firstEditionTokenId, _0_1_ETH, '0', {from: contract});
+      await this.primaryMarketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorA);
+
+      // lists and sell from A -> B
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorA});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorB, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorB);
+
+      // list as reserve and sell B -> C
+      await this.marketplace.listForReserveAuction(collectorB, firstEditionTokenId, ether('0.25'), '0', {from: collectorB});
+      await this.marketplace.placeBidOnReserveAuction(firstEditionTokenId, {from: collectorC, value: ether('0.3')});
+
+      // increase time beyond end
+      const {biddingEnd} = await this.marketplace.editionOrTokenWithReserveAuctions(firstEditionTokenId);
+      await time.increaseTo(biddingEnd);
+
+      // transfer the token post bidding end -> sends from B -> C
+      await this.token.transferFrom(collectorB, collectorC, firstEditionTokenId, {from: collectorB});
+
+      // trigger emergency exit to return funds
+      await this.marketplace.emergencyExitBidFromReserveAuction(firstEditionTokenId, {from: contract});
+
+      // C lists item but then transfer to D
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorC});
+      await this.token.transferFrom(collectorC, collectorD, firstEditionTokenId, {from: collectorC});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorD);
+
+      // D final lists and sells back to A
+      await this.marketplace.listTokenForBuyNow(firstEditionTokenId, _0_1_ETH, '0', {from: collectorD});
+      await this.marketplace.buyEditionToken(firstEditionTokenId, {from: collectorA, value: _0_1_ETH});
+      expect(await this.token.ownerOf(firstEditionTokenId)).to.be.equal(collectorA);
+    });
+  });
 });
