ce1d69bf3e17aae92b759a759ca68f1f1086800a
Liam Freeman <liamfreeman@admins-MacBook-Pro.local>
2022-01-14 11:30:31 +0000

    fixed time in tests and added some tests for mint from sale

--- contracts/gatedsale/BasicGatedSale.sol
+++ contracts/gatedsale/BasicGatedSale.sol
@@ -14,9 +14,9 @@ contract BasicGatedSale {
         uint256 end;
         uint256 mints;
         uint256 mintLimit;
-        address[] preList;
     }
 
+    mapping(uint256 => mapping(address => bool)) public preList;
     mapping(uint256 => Sale) public sales;
 
     constructor() {}
@@ -32,22 +32,43 @@ contract BasicGatedSale {
         require(_mints > 0, 'total mints must be greater than 0');
         require(_addresses.length > 0, 'addresses count must be greater than 0');
 
+        // If no mint limit is provided then set it to the size of the sale
         if(_mintLimit == 0) _mintLimit = _mints;
 
+        // Get the latest sale ID
         uint256 id = _getLatestID();
 
-        sales[id] = Sale(id, _start, _end, _mints, _mintLimit, _addresses);
+        // Create the sale
+        sales[id] = Sale(id, _start, _end, _mints, _mintLimit);
+
+        // Loop over the addresses and store who is on the pre list
+        for(uint i = 0; i < _addresses.length; i++) {
+            preList[id][_addresses[i]] = true;
+        }
     }
 
-    function mintFromSale(uint256 _saleID, uint256 _mintCount) public {
+    function getMintingStatus(uint256 _saleID, address _mintingAddress) public view returns (bool) {
+        return preList[_saleID][_mintingAddress];
+    }
+
+    function mintFromSale(uint256 _saleID, address _mintingAddress, uint256 _mintCount) public {
+        // Check if the address is present in the prelist
+        if(!getMintingStatus(_saleID, _mintingAddress)) {
+            revert('address not able to mint from sale');
+        }
+
+        // Get the sale object
         Sale storage sale = sales[_saleID];
 
         require(sale.id != 0, 'sale does not exist');
-        require(_mintCount > 0 && _mintCount < sale.mintLimit, 'number of mints must be below mint limit');
+        require(sale.mints != 0, 'sale is sold out');
+        require(sale.start < block.timestamp, 'sale has not started yet');
+        require(sale.end < block.timestamp, 'sale has ended');
+        require(_mintCount > 0 && _mintCount <= sale.mintLimit, 'number of mints must be below mint limit');
 
         sale.mints = sale.mints - _mintCount;
 
-        // TODO trigger some sort of NFT transfer on mint
+        // TODO trigger some sort of NFT transfer on mint and emit event
     }
 
     // set up sale so each sale has it's own prelist and we can prove who is on it...
--- test/gatedsale/BasicGatedSale.test.js
+++ test/gatedsale/BasicGatedSale.test.js
@@ -1,68 +1,150 @@
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
-const {expectRevert} = require('@openzeppelin/test-helpers');
+const {expectRevert, time} = require('@openzeppelin/test-helpers');
+const {BigNumber} = require("ethers");
 
 const BasicGatedSale = artifacts.require('BasicGatedSale');
 
+async function mockTime() {
+    const timeNow = await time.latest()
+
+    const saleStart = new Date(Number(timeNow.toString()));
+    saleStart.setDate(saleStart.getDate() + 1);
+    const saleEnd = new Date(Number(timeNow.toString()));
+    saleEnd.setDate(saleEnd.getDate() + 2);
+
+    return {
+        timeNow: timeNow,
+        saleStart: BigNumber.from(saleStart.getTime()),
+        saleEnd: BigNumber.from(saleEnd.getTime())
+    }
+}
+
 contract('BasicGatedSale Test Tests...', function (accounts) {
 
-    const currentTime = Math.floor(new Date().getTime() / 1000)
-    const [admin, andy, liam, james, dave, bob] = accounts;
+    const [admin, andy, liam, james] = accounts;
 
-    describe.only('BasicGatedSale', () => {
+    describe.only('BasicGatedSale', async () => {
 
         context('createSale', async () => {
 
             it('can create a new sale with correct arguments', async () => {
+                const {saleStart, saleEnd} = await mockTime()
+
                 const basicGatedSale = await BasicGatedSale.new();
 
-                await basicGatedSale.createSale(currentTime + 100, currentTime + 200, 100, 1, [liam, andy])
+                await basicGatedSale.createSale(saleStart, saleEnd, 100, 1, [liam, andy])
 
-
-                const {id, start, end, mints, mintLimit, preList} = await basicGatedSale.sales(1)
+                const {id, start, end, mints, mintLimit} = await basicGatedSale.sales(1)
 
                 expect(id.toString()).to.be.equal('1')
-                expect(start.toString()).to.be.equal(`${currentTime + 100}`)
-                expect(end.toString()).to.be.equal(`${currentTime + 200}`)
+                expect(start.toString()).to.be.equal(`${saleStart.toString()}`)
+                expect(end.toString()).to.be.equal(`${saleEnd.toString()}`)
                 expect(mints.toString()).to.be.equal('100')
                 expect(mintLimit.toString()).to.be.equal('1')
+
+                expect(await basicGatedSale.getMintingStatus(1, liam)).to.be.true
+                expect(await basicGatedSale.getMintingStatus(1, andy)).to.be.true
             })
 
             it('will revert if given start date which is before the current block timestamp', async () => {
+                let {timeNow, saleEnd} = await mockTime()
+
+                const saleStart = new Date(Number(timeNow.toString()));
+                saleStart.setDate(saleStart.getDate() - 4);
+
                 const basicGatedSale = await BasicGatedSale.new();
 
                 await expectRevert(
-                    basicGatedSale.createSale(currentTime - 500, currentTime + 500, 100, 1, [liam, andy]),
+                    basicGatedSale.createSale(BigNumber.from(saleStart.getTime()), saleEnd, 100, 1, [liam, andy]),
                     'sale start time must be in the future'
                 )
             })
 
             it('will revert if given an end date before the start date', async () => {
+                let {timeNow, saleStart} = await mockTime()
+
+                const saleEnd = new Date(Number(timeNow.toString()));
+                saleEnd.setDate(saleEnd.getDate() - 4);
+
                 const basicGatedSale = await BasicGatedSale.new();
 
                 await expectRevert(
-                    basicGatedSale.createSale(currentTime + 500, currentTime - 500, 100, 1, [liam, andy]),
+                    basicGatedSale.createSale(saleStart, BigNumber.from(saleEnd.getTime()), 100, 1, [liam, andy]),
                     'sale end time must be after the sale start time'
                 )
             })
 
             it('will revert if a mints number less than 1', async () => {
+                const { saleStart, saleEnd} = await mockTime()
+
                 const basicGatedSale = await BasicGatedSale.new();
 
                 await expectRevert(
-                    basicGatedSale.createSale(currentTime + 100, currentTime + 200, 0, 1, [liam, andy]),
+                    basicGatedSale.createSale(saleStart, saleEnd, 0, 1, [liam, andy]),
                     'total mints must be greater than 0'
                 )
             })
 
             it('will revert if a not given any addresses', async () => {
+                const { saleStart, saleEnd} = await mockTime()
+
                 const basicGatedSale = await BasicGatedSale.new();
 
                 await expectRevert(
-                    basicGatedSale.createSale(currentTime + 100, currentTime + 200, 100, 1, []),
+                    basicGatedSale.createSale(saleStart, saleEnd, 10, 1, []),
                     'addresses count must be greater than 0'
                 )
             })
         })
-    })
+
+        context('mintFromSale', async () => {
+            it('can mint one item from a valid sale', async () => {
+                const { saleStart, saleEnd} = await mockTime()
+
+                const basicGatedSale = await BasicGatedSale.new();
+                await basicGatedSale.createSale(saleStart, saleEnd, 5, 1, [liam, andy])
+
+                await time.increase(saleStart.toString())
+                await time.increase(time.duration.hours(1))
+
+                await basicGatedSale.mintFromSale(1, liam, 1)
+
+                const {mints} = await basicGatedSale.sales(1)
+
+                expect(mints.toString()).to.be.equal('4')
+            })
+
+            it('can mint multiple items from a valid sale', async () => {
+                const { saleStart, saleEnd} = await mockTime()
+
+                const basicGatedSale = await BasicGatedSale.new();
+                await basicGatedSale.createSale(saleStart, saleEnd, 5, 3, [liam, andy])
+
+                await time.increase(saleStart.toString())
+                await time.increase(time.duration.hours(1))
+
+                await basicGatedSale.mintFromSale(1, liam, 3)
+
+                const {mints} = await basicGatedSale.sales(1)
+
+                expect(mints.toString()).to.be.equal('2')
+            })
+
+            it('reverts if the address is not in the pre list', async () => {
+                const { saleStart, saleEnd} = await mockTime()
+
+                const basicGatedSale = await BasicGatedSale.new();
+                await basicGatedSale.createSale(saleStart, saleEnd, 5, 3, [liam, andy])
+
+                await time.increase(saleStart.toString())
+                await time.increase(time.duration.hours(1))
+
+                await expectRevert(
+                    basicGatedSale.mintFromSale(1, james, 1),
+                    'address not able to mint from sale'
+                )
+            })
+        })
+    });
 });
