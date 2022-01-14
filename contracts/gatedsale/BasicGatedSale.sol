19190dcce1ee0cf43223f63d2179ccda84e8bf60
Andy Gray <andy.gray@blockrocket.tech>
2022-01-14 12:44:54 +0000

    comments and fixmes

--- contracts/gatedsale/BasicGatedSale.sol
+++ contracts/gatedsale/BasicGatedSale.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.4;
 
 contract BasicGatedSale {
 
-    uint256 idCounter;
+    uint256 idCounter; // FIXME should this be public or private?
 
     struct Sale {
         uint256 id;
@@ -13,11 +13,12 @@ contract BasicGatedSale {
         uint256 mintLimit;
     }
 
-    mapping(uint256 => mapping(address => bool)) public preList;
+    mapping(uint256 => mapping(address => bool)) public preList; //FIXME could argue this could be private and use the getter?
     mapping(uint256 => Sale) public sales;
 
     constructor() {}
 
+    // FIXME next ID?
     function _getLatestID() private returns (uint256) {
         idCounter = idCounter + 1;
         return idCounter;
@@ -36,20 +37,25 @@ contract BasicGatedSale {
         uint256 id = _getLatestID();
 
         // Create the sale
-        sales[id] = Sale(id, _start, _end, _mints, _mintLimit);
+        sales[id] = Sale(id, _start, _end, _mints, _mintLimit); // FIXME maybe use the way where the vars are specified as you might pass in the wrong order
 
         // Loop over the addresses and store who is on the pre list
         for(uint i = 0; i < _addresses.length; i++) {
             preList[id][_addresses[i]] = true;
         }
+
+        // FIXME think about adding an event here
     }
 
     function getMintingStatus(uint256 _saleID, address _mintingAddress) public view returns (bool) {
         return preList[_saleID][_mintingAddress];
     }
 
+    // FIXME use msg.sender instead of passing _mintingAddress
     function mintFromSale(uint256 _saleID, address _mintingAddress, uint256 _mintCount) public {
         // Check if the address is present in the prelist
+        // FIXME you can add this check direct in the require
+        // FIXME should be require rather than revert
         if(getMintingStatus(_saleID, _mintingAddress) != true) {
             revert('address not able to mint from sale');
         }
@@ -58,10 +64,11 @@ contract BasicGatedSale {
         Sale storage sale = sales[_saleID];
 
         require(sale.mints != 0, 'sale is sold out');
-        require(block.timestamp > sale.start, 'sale has not started yet');
+        require(block.timestamp > sale.start, 'sale has not started yet'); // FIXME >= maybe?
         require(block.timestamp < sale.end, 'sale has ended');
         require(_mintCount > 0 && _mintCount <= sale.mintLimit, 'number of mints must be below mint limit');
 
+        // FIXME this is wrong - mints is the amount per person generically so I suspect you may need a new mapping
         sale.mints = sale.mints - _mintCount;
 
         // TODO trigger some sort of NFT transfer on mint and emit event
