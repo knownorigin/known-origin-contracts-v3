fae45239b58848789a611f51b78d308dfff56303
Liam Freeman <liamfreeman@admins-MacBook-Pro.local>
2022-01-14 12:05:09 +0000

    removed imports

--- contracts/gatedsale/BasicGatedSale.sol
+++ contracts/gatedsale/BasicGatedSale.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "hardhat/console.sol";
-import "../spikes/collaborators/IFundsDrainable.sol";
-
 contract BasicGatedSale {
 
     uint256 idCounter;
