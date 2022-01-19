60ffba908281e256e3a0d647bc670d092a429b7d
Liam Freeman <liamfreeman@admins-MacBook-Pro.local>
2022-01-19 11:05:41 +0000

    refactored to one function that creates sale and phase, renamed onprelist to canmint

--- contracts/gatedsale/BasicGatedSale.sol
+++ contracts/gatedsale/BasicGatedSale.sol
@@ -13,48 +13,46 @@ import "../marketplace/IKODAV3Marketplace.sol";
 
 contract BasicGatedSale is BaseMarketplace {
 
-    event SaleCreated(uint256 indexed id);
+    event SaleCreated(uint256 indexed id, uint256 editionID);
+    event PhaseCreated(uint256 indexed saleID, uint256 editionID, uint256 startTime, uint256 endTime, uint256 mintLimit, bytes32 merkleRoot, string dataIPFSHash, uint256 priceInWei);
     event MintFromSale(uint256 saleID, address account, uint256 mintCount);
 
     uint256 private saleIdCounter;
 
-    // TODO is phase a better struct name?
-    struct SalePhase {
+    /// @notice Phase represents a time structured part of a sale, i.e. VIP, pre sale or open sale
+    struct Phase {
         uint256 startTime; // The start time of the sale as a whole
         uint256 endTime; // The end time of the sale phase, also the beginning of the next phase if applicable
         uint256 mintLimit; // The mint limit per wallet for the phase
         bytes32 merkleRoot; // The merkle tree root for the phase
-        // TODO add IPFS hash for reference to merkle in IPFS   ie  string dataIPFSHash;
+        string dataIPFSHash; // The IPFS hash referencing the merkle tree
         uint256 priceInWei; // Price in wei for one mint
     }
 
+    /// @notice Sale represents a gated sale, with mapping links to different sale phases
     struct Sale {
         uint256 id; // The ID of the sale
-        uint256 editionId;
+        uint256 editionId; // The ID of the edition the sale will mint
     }
 
     /// @notice KO commission on every sale
     uint256 public platformPrimarySaleCommission = 15_00000;  // 15.00000%
 
     mapping(uint256 => Sale) public sales;
-    mapping(uint256 => SalePhase[]) public phases; // TODO had to make this public for testing?
+    mapping(uint256 => Phase[]) public phases;
     mapping(uint256 => mapping(uint256 => mapping(address => uint))) private totalMints;
 
     constructor(IKOAccessControlsLookup _accessControls, IKODAV3 _koda, address _platformAccount)
         BaseMarketplace(_accessControls, _koda, _platformAccount) {
     }
 
-    // TODO move private functions to the bottom - public first
-    // _nextSaleID generates the next available SaleID
-    function _nextSaleId() private returns (uint256) {
-        saleIdCounter += 1;
-        return saleIdCounter;
-    }
-
-    // TODO so only one transaction we should maybe do createSaleWithPhase and make this an internal function
-    function createSale(uint256 _editionId) public onlyAdmin {
-
-        // TODO check _editionId exists via KODA? Nice to have?
+    function createSaleWithPhase(uint256 _editionId, uint256 _startTime, uint256 _endTime, uint256 _mintLimit, bytes32 _merkleRoot, uint256 _priceInWei) public onlyAdmin {
+        require(koda.editionExists(_editionId), 'edition does not exist');
+        require(_startTime > block.timestamp, 'phase start time must be in the future');
+        require(_endTime > _startTime, 'phase end time must be after start time');
+        require(_mintLimit > 0, 'phase mint limit must be greater than 0');
+        console.log('MERKLE ROOT LENGTH : ', _merkleRoot.length);
+        require(_merkleRoot.length != 0, 'phase must have a valid merkle root'); // TODO is this right?
 
         // Get the latest sale ID
         uint256 saleId = _nextSaleId();
@@ -62,31 +60,22 @@ contract BasicGatedSale is BaseMarketplace {
         // Assign the sale to the sales mapping
         sales[saleId] = Sale({id : saleId, editionId : _editionId});
 
-        emit SaleCreated(saleId); // TODO add edition in here
+        emit SaleCreated(saleId, _editionId);
+
+        phases[saleId].push(
+            Phase({
+        startTime : _startTime,
+        endTime : _endTime,
+        mintLimit : _mintLimit,
+        merkleRoot : _merkleRoot,
+        priceInWei : _priceInWei
+        }));
+
+        emit PhaseCreated(saleId, _editionId, _startTime, _endTime, _mintLimit, _merkleRoot, _priceInWei);
     }
 
-    function addPhase(uint256 _saleId, uint256 _startTime, uint256 _endTime, uint256 _mintLimit, bytes32 _merkleRoot, uint256 _priceInWei) public onlyAdmin {
-        require(_startTime > block.timestamp, 'phase start time must be in the future'); // TODO prob always true but should we enforce it?
-        require(_endTime > _startTime, 'phase end time must be after start time');
-        require(_mintLimit > 0, 'phase mint limit must be greater than 0');
-        require(_merkleRoot != 0, 'phase must have a valid merkle root'); // TODO check is this a valid empty bytes check?
-        require(_priceInWei > 0, 'phase price must be greater than 0'); // TODO I would remove; I can see artists have free mints in phases for collectors
-
-        phases[_saleId].push(
-            SalePhase({
-            startTime : _startTime,
-            endTime : _endTime,
-            mintLimit : _mintLimit,
-            merkleRoot : _merkleRoot,
-            priceInWei : _priceInWei
-            }));
-
-        // TODO add event
-    }
-
-    // TODO I don't like this name - maybe just mint?
-    function mintFromSale(uint256 _saleId, uint256 _salePhaseId, uint256 _mintCount, uint256 _index, bytes32[] calldata _merkleProof) payable public nonReentrant {
-        SalePhase memory phase = phases[_saleId][_salePhaseId];
+    function mint(uint256 _saleId, uint256 _salePhaseId, uint256 _mintCount, uint256 _index, bytes32[] calldata _merkleProof) payable public nonReentrant {
+        Phase memory phase = phases[_saleId][_salePhaseId];
 
         // Check the phase exists and it is in progress
         require(block.timestamp >= phase.startTime && block.timestamp < phase.endTime, 'sale phase not in progress');
@@ -95,7 +84,7 @@ contract BasicGatedSale is BaseMarketplace {
         require(msg.value >= phase.priceInWei * _mintCount, 'not enough wei sent to complete mint');
 
         // Check the msg sender is on the pre list
-        require(onPreList(_saleId, _salePhaseId, _index, msg.sender, _merkleProof), 'address not able to mint from sale');
+        require(canMint(_saleId, _salePhaseId, _index, msg.sender, _merkleProof), 'address not able to mint from sale');
 
         require(totalMints[_saleId][_salePhaseId][msg.sender] + _mintCount <= phase.mintLimit, 'cannot exceed total mints for sale phase');
 
@@ -106,7 +95,7 @@ contract BasicGatedSale is BaseMarketplace {
         (address receiver, address creator, uint256 tokenId) = koda.facilitateNextPrimarySale(sale.editionId);
 
         // split money
-        _handleEditionSaleFunds(sale.editionId, creator, receiver, phase.priceInWei * _mintCount); // FIXME should be mint total * price - refunds? Or just send msg.value for now? As ETH would be left in contract?
+        _handleEditionSaleFunds(sale.editionId, creator, receiver, msg.value);
 
         // send token to buyer (assumes approval has been made, if not then this will fail)
         koda.safeTransferFrom(creator, msg.sender, tokenId);
@@ -114,31 +103,30 @@ contract BasicGatedSale is BaseMarketplace {
         emit MintFromSale(_saleId, msg.sender, _mintCount);
     }
 
-    // TODO one for discussion - might need an internal and a public for gas reasons - discuss with James/Vince
-    // TODO is prelist one word or two?
-    function onPreList(uint256 _saleId, uint _salePhaseId, uint256 _index, address _account, bytes32[] calldata _merkleProof) public view returns (bool) {
-        SalePhase memory phase = phases[_saleId][_salePhaseId];
+    function canMint(uint256 _saleId, uint _salePhaseId, uint256 _index, address _account, bytes32[] calldata _merkleProof) public view returns (bool) {
+        Phase memory phase = phases[_saleId][_salePhaseId];
 
         // assume balance of 1 for enabled artists
         bytes32 node = keccak256(abi.encodePacked(_index, _account, uint256(1)));
         return MerkleProof.verify(_merkleProof, phase.merkleRoot, node);
     }
 
-    // shall we use?
-    function _processSale(
-        uint256 _tokenId,
-        uint256 _paymentAmount,
-        address _buyer,
-        address _seller
-    ) internal override returns (uint256) {
-//        _facilitateSecondarySale(_tokenId, _paymentAmount, _seller, _buyer);
-        return _tokenId;
-    }
-
-    // not used
-    function _isListingPermitted(uint256 _editionId) internal view override returns (bool) {
-        return false;
-    }
+    // TODO are these needed?
+//    // shall we use?
+//    function _processSale(
+//        uint256 _tokenId,
+//        uint256 _paymentAmount,
+//        address _buyer,
+//        address _seller
+//    ) internal override returns (uint256) {
+////        _facilitateSecondarySale(_tokenId, _paymentAmount, _seller, _buyer);
+//        return _tokenId;
+//    }
+//
+//    // not used
+//    function _isListingPermitted(uint256 _editionId) internal view override returns (bool) {
+//        return false;
+//    }
 
     function _handleEditionSaleFunds(uint256 _editionId, address _creator, address _receiver, uint256 _paymentAmount) internal {
         uint256 koCommission = (_paymentAmount / modulo) * platformPrimarySaleCommission;
@@ -147,7 +135,7 @@ contract BasicGatedSale is BaseMarketplace {
             require(koCommissionSuccess, "Edition commission payment failed");
         }
 
-        (bool success,) = _receiver.call{value : _paymentAmount - koCommission}("");
+        (bool success) = _receiver.call{value : _paymentAmount - koCommission}("");
         require(success, "Edition payment failed");
     }
 
@@ -155,5 +143,11 @@ contract BasicGatedSale is BaseMarketplace {
     function updatePlatformPrimarySaleCommission(uint256 _platformPrimarySaleCommission) public onlyAdmin {
         platformPrimarySaleCommission = _platformPrimarySaleCommission;
         emit AdminUpdatePlatformPrimarySaleCommission(_platformPrimarySaleCommission);
+    }
+
+    /// @dev _nextSaleID generates the next available SaleID
+    function _nextSaleId() private returns (uint256) {
+        saleIdCounter += 1;
+        return saleIdCounter;
     }
 }
