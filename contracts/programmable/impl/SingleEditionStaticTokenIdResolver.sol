3600426a9803ac672f437c0b73500a0e328bfb80
James Morgan <james.morgan.e@googlemail.com>
2022-03-10 08:20:50 +0000

    Adding initial pass of LCG

--- contracts/programmable/impl/SingleEditionStaticTokenIdResolver.sol
+++ contracts/programmable/impl/SingleEditionStaticTokenIdResolver.sol
@@ -3,22 +3,15 @@
 pragma solidity 0.8.4;
 
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {ITokenUriResolver} from "../ITokenUriResolver.sol";
-import {IKODAV3} from "../../core/IKODAV3.sol";
+import {BaseTokenUriResolver} from "./BaseTokenUriResolver.sol";
+import {UrlTools} from "./UrlTools.sol";
 
-contract SingleEditionStaticTokenIdResolver is ITokenUriResolver {
-    using Strings for uint256;
+contract SingleEditionStaticTokenIdResolver is BaseTokenUriResolver {
 
-    string baseStorageHash;
     string joiner;
-    string extension;
+    string public baseStorageHash;
 
-    function tokenURI(uint256, uint256 _tokenId) external override view returns (string memory) {
-        // {baseStorageHash}{joiner}{tokenId} e.g. ipfs://ipfs/Qmd9xQFBfqMZLG7RA2rXor7SA7qyJ1Pk2F2mSYzRQ2siMv/1234
-        return string(abi.encodePacked(baseStorageHash, joiner, _tokenId.toString()));
-    }
-
-    function isDefined(uint256, uint256) external override view returns (bool) {
-        return true;
+    function tokenURI(uint256 _editionId, uint256 _tokenId) external override view returns (string memory) {
+        return UrlTools.staticTokenUri(_tokenId, baseStorageHash, joiner);
     }
 }
